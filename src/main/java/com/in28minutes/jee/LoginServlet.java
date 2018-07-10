3a47ad592a51f2caf4add827bf535fb7f79f39e1
ghazouan badr <bghazouan9@gmail.com>
2018-07-10 16:01:30 +0100

    first one

---
+++ src/main/java/com/in28minutes/jee/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.in28minutes.jee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginServlet {
+
+	private LoginService service = new LoginService();
+
+	@ResponseBody
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String redirect() {
+
+		return "hello world due to response body";
+	}
+
+	/*protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+
+		boolean isValidUser = service.validateUser(name, password);
+
+		if (isValidUser) {
+			request.setAttribute("name", name);
+			request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(
+					request, response);
+		} else {
+			request.setAttribute("errorMessage", "Invalid Credentials!!");
+			request.getRequestDispatcher("login").forward(
+					request, response);
+		}
+	}*/
+
+}
